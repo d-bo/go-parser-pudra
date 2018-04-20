de2b7696e5e074d89d54c8ad23ab76dc0790e7a9
Болховитинов Дмитрий <bolhovitinov_d@goldapple.ru>
2018-04-20 18:56:07 +0500

    parse brands

---
+++ get.sh
@@ -0,0 +1,7 @@
+export GOPATH=`pwd`
+go get gopkg.in/mgo.v2
+go get github.com/gin-gonic/gin
+go get github.com/go-redis/redis
+go get github.com/blackjack/syslog
+go get github.com/parnurzeal/gorequest
+go get github.com/json-iterator/go
--- pudra.go
+++ pudra.go
@@ -1,4 +1,4 @@
-package gapple
+package main
 
 import (
     //"os"
@@ -12,14 +12,12 @@ import (
     //"encoding/json"
     //"io/ioutil"
     "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
+    //"gopkg.in/mgo.v2/bson"
     "golang.org/x/net/html"
     "github.com/go-redis/redis"
-    "github.com/json-iterator/go"
+    //"github.com/json-iterator/go"
     "github.com/blackjack/syslog"
-    "github.com/robertkrimen/otto"
     "github.com/parnurzeal/gorequest"
-    "github.com/robertkrimen/otto/parser"
 )
 
 // A time prefix before collection name
@@ -108,61 +106,38 @@ func ExtractLinks(glob_session *mgo.Session, url string, redis_cli *redis.Client
 }
 
 // Extract breadcrumbs
-func ExtractNavi(glob_session *mgo.Session, url string, redis_cli *redis.Client) {
+func ExtractBrand(glob_session *mgo.Session, url string, redis_cli *redis.Client) {
 
     var f func(*html.Node, *mgo.Session, *redis.Client)
-    var f1 func(*html.Node, *mgo.Session)
-    var f2 func(*html.Node, *mgo.Session)
-    var f3 func(*html.Node, *mgo.Session)
-    var f4 func(*html.Node, *mgo.Session, *redis.Client)
 
-    var crumbs []string
-    var crumbsTail string
-    var contents string
-    var finalCrumb string
-    var tails []string
-    var newone, double int
-
-    collect := glob_session.DB("parser").C("auchan_navi")
-
-    newone = 0
-    double = 0
-    var ProdName string
+    //var crumbs []string
+    //collect := glob_session.DB("parser").C("pudra_navi")
 
     f = func(node *html.Node, session *mgo.Session, redis_cli *redis.Client) {
-        if node.Type == html.ElementNode && node.Data == "ul" {
+        if node.Type == html.ElementNode && node.Data == "a" {
+            match := false
+            contents := ""
+            href := ""
+            _ = href
             for _, a := range node.Attr {
                 if a.Key == "class" {
-                    if strings.Contains(a.Val, "breadcrumbs__list") {
-                        f1(node, session)
-                        f2(node, session)
-                        if crumbsTail != "" {
-                            crumbs = append(crumbs, crumbsTail)
+                    if strings.Contains(a.Val, "b-menu-item-link") {
+                        contents = renderNode(node)
+                        contents = extractContext(contents)
+                        contents = strings.Replace(contents, "\n", "", -1)
+                        contents = strings.Replace(contents, "\r", "", -1)
+                        contents = strings.Replace(contents, "\t", "", -1)
+                        if len(contents) > 1 {
+                            match = true
                         }
-                        finalCrumb = strings.Join(crumbs, ";")
-
-                        for i := 0; i < len(tails); i++ {
-                            // Insert Mongo
-                            // check double product
-                            ProdName = tails[i]
-                            num, err := collect.Find(bson.M{"name": ProdName, "navi": finalCrumb}).Count()
-                            if err != nil {
-                                syslog.Critf("auchan error find by code: %s", err)
-                            }
-                            if num < 1 {
-                                collect.Insert(bson.M{"navi": finalCrumb, "name": tails[i]})
-                                newone++
-                            } else {
-                                fmt.Println("DOUBLE")
-                                double++
-                            }
-                            fmt.Println("NAVI", finalCrumb)
-                            fmt.Println("NAME", tails[i])
-                        }
-                        tails = []string{}
-                        ProdName = ""
                     }
                 }
+                if a.Key == "href" {
+                    href = a.Val
+                }
+            }
+            if match {
+                fmt.Println("FOUND b-menu-item-link", contents, len(contents), href)
             }
         }
 
@@ -171,109 +146,7 @@ func ExtractNavi(glob_session *mgo.Session, url string, redis_cli *redis.Client)
         }
     }
 
-    // Bc active links
-    f1 = func(node *html.Node, session *mgo.Session) {
-        if node.Type == html.ElementNode && node.Data == "a" {
-            for _, a := range node.Attr {
-                if a.Key == "href" {
-                    if strings.Contains(a.Val, "pokupki") {
-                        contents = renderNode(node)
-                        contents = extractContext(contents)
-                        if !strings.Contains(contents, "Страница") {
-                            crumbs = append(crumbs, contents)
-                        }
-                    }
-                }
-            }
-        }
-
-        for c := node.FirstChild; c != nil; c = c.NextSibling {
-            f1(c, session)
-        }
-    }
-
-    // Breadcrumbs tail
-    f2 = func(node *html.Node, session *mgo.Session) {
-        if node.Type == html.ElementNode && node.Data == "strong" {
-            contents = renderNode(node)
-            contents = extractContext(contents)
-            if !strings.Contains(contents, "Страница") {
-                crumbsTail = contents
-            }
-        }
-
-        for c := node.FirstChild; c != nil; c = c.NextSibling {
-            f2(c, session)
-        }
-    }
-
-    // Crumbs active links
-    // JSON Extract name from this
-    /*
-        {
-            "id": "145666",
-            "type": "simple",
-            "name": "Туалетная вода «Cool Water» Davidoff, 75 мл ",
-            "brand": "not_set",
-            "category": "Мужские ароматы",
-            "list": "Catalog Page",
-            "position": "5",
-            "listPosition": "4"
-        },
-        {
-            "id": "124546",
-            "type": "simple",
-            "name": "Туалетная вода «Gucci By Gucci Pour Homme» Gucci, 50 мл",
-            "brand": "not_set",
-            "category": "Мужские ароматы",
-            "list": "Catalog Page",
-            "position": "6",
-            "listPosition": "5"
-        },
-    */
-
-    f3 = func(node *html.Node, session *mgo.Session) {
-        if node.Type == html.ElementNode && node.Data == "script" {
-            contents = renderNode(node)
-            contents = extractContext(contents)
-            if strings.Contains(contents, "staticImpressions['product_list']") {
-                tails = ExtractJSON(contents)
-            }
-        }
-
-        for c := node.FirstChild; c != nil; c = c.NextSibling {
-            f3(c, session)
-        }
-    }
-
-    // Detect pagination
-    // Push "next" link to redis queue
-    f4 = func(node *html.Node, session *mgo.Session, redis_cli *redis.Client) {
-        if node.Type == html.ElementNode && node.Data == "link" {
-            match := false
-            var value string
-            for _, a := range node.Attr {
-                if a.Key == "href" {
-                    value = a.Val
-                }
-                if a.Val == "next" {
-                    match = true
-                }
-            }
-
-            if match {
-                fmt.Println("PUSH/EXTRACT", value)
-                ExtractNavi(session, value, redis_cli)
-                match = false
-            }
-        }
-
-        for c := node.FirstChild; c != nil; c = c.NextSibling {
-            f4(c, session, redis_cli)
-        }
-    }
-
-    fmt.Println("#ExtractNavi:", url)
+    fmt.Println("REQUEST URL:", url)
     request := gorequest.New()
     resp, body, errs := request.Get(url).
         Retry(3, 5 * time.Second, http.StatusBadRequest, http.StatusInternalServerError).
@@ -289,93 +162,23 @@ func ExtractNavi(glob_session *mgo.Session, url string, redis_cli *redis.Client)
         syslog.Critf("auchan html.Parse error: %s", errs)
     }
 
-    f3(doc, glob_session)
-    f4(doc, glob_session, redis_cli) // Pagination push
     f(doc, glob_session, redis_cli)
-    fmt.Println("NEW", newone, "DOUBLE", double)
-}
-
-func ExtractJSONOtto(context string) string {
-    vm := otto.New()
-    vm.Run(context)
-    if value, err := vm.Get("staticImpressions"); err == nil {
-        fmt.Println("staticImpressions", value)
-    }
-    filename := ""
-    program, err := parser.ParseFile(nil, filename, context, 0)
-    if err != nil {
-        fmt.Println(err)
-    }
-    for _, ll := range program.DeclarationList {
-        fmt.Println("PROGRAM: ", ll)
-    }
-
-    return "0"
-}
-
-// Extract JSON
-// Breadcrumb tails
-func ExtractJSON(context string) []string {
-
-    var i int
-    var rec bool
-    var strbuff []byte
-    var dat interface{}
-    var needle = "staticImpressions['product_list']"
-    var Names []string
-
-    num := strings.Index(context, needle)
-
-    rec = false
-    for i = (num + len(needle) + 1); i < len(context); i++ {
-        // Start
-        if string(context[i]) == "[" && rec == false {
-            fmt.Println("Start [", i)
-            rec = true
-        }
-        // Stop
-        if string(context[i]) == "]" && rec == true {
-            fmt.Println("Stop ]", i)
-            break
-        }
-        if rec == true {
-            strbuff = append(strbuff, context[i])
-        }
-    }
-
-    var json = jsoniter.ConfigCompatibleWithStandardLibrary
-    strbuffstr := []byte(string(strbuff)+"{}]")
-    json.Unmarshal(strbuffstr, &dat)
-
-    for k, v := range dat.([]interface{}) {
-        switch t := v.(type) {
-            case interface{}:
-                for ck, cv := range t.(map[string]interface{}) {
-                    if strings.Contains(ck, "name") {
-                        Names = append(Names, cv.(string))
-                    }
-                }
-            default:
-                fmt.Println("wrong type")
-        }
-        //fmt.Printf("key[%s] value[%s]\n", k, v)
-        _ = k
-        _ = v
-    }
-
-    return Names
 }
 
 func main() {
 
+    // Redis
     client := redis.NewClient(&redis.Options{
         Addr:     "localhost:6379",
         Password: "", // no password set
         DB:       0,  // use default DB
     })
-
     pong, err := client.Ping().Result()
     fmt.Println(pong, err)
+
+    // Mongo
+    session, glob_err := mgo.Dial("mongodb://apidev:apidev@localhost:27017/parser")
+    defer session.Close()
 
     pubsub := client.Subscribe("productPageLinkChannel")
     defer pubsub.Close()
@@ -386,28 +189,19 @@ func main() {
     }
     fmt.Println(subscr)
 
-    session, glob_err := mgo.Dial("mongodb://apidev:apidev@localhost:27017/parser")
-    defer session.Close()
-
     if glob_err != nil {
         syslog.Critf("Error: %s", glob_err)
     }
-    if DB == "" {
-        DB = "parser"
-    }
 
-    gapple.ExtractLinks(session, "https://www.auchan.ru/pokupki/hoztovary.html", client, "hoztovary")
-    gapple.ExtractLinks(session, "https://www.auchan.ru/pokupki/kosmetika/dlja-muzhchin/muzhskie-aromaty.html", client, "kosmetika")
-    gapple.ExtractLinks(session, "https://www.auchan.ru/pokupki/deti.html", client, "deti")
+    ExtractBrand(session, `https://pudra.ru/brands.html`, client)
 
-    //gapple.ExtractNavi(session, "https://www.auchan.ru/pokupki/kosmetika/dlja-muzhchin.html", client)
-
+    /*
     for {
         msg, err := pubsub.ReceiveMessage()
         if err != nil {
             fmt.Println("ERROR: ", err)
         }
         fmt.Println(msg.Channel, " MSG RCV: ", msg.Payload)
-        gapple.ExtractNavi(session, msg.Payload, client)
     }
+    */
 }
